08df8a620649a17faa51da5f33c73a4f05d12321
malaaaaaaaa <134796611+malaaaaaaaa@users.noreply.github.com>
2023-05-27 17:31:31 +0000

    Updated 171

--- Python/0171_ExcelSheetColumnNumber.py
+++ Python/0171_ExcelSheetColumnNumber.py
@@ -2,6 +2,6 @@ class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         result = 0
         for char in columnTitle:
-            value = ord(char) - ord('A') + 1
+            value = ord(char) - ord('A') + 1 # First time using ord and did not know what it did until reading about it
             result = result*26 + value
         return result
